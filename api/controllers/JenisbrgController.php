03e3e239e672e581bdb654e3bd922522cd0b5bba
x1n73ct <x1n73ct@x1n73ct>
2015-06-20 14:02:14 +0700

    Merge origin/master
    Conflicts:
    	tpl/blocks/pagination.custom.html

--- api/controllers/JenisbrgController.php
+++ api/controllers/JenisbrgController.php
@@ -3,7 +3,7 @@
 namespace app\controllers;
 
 use Yii;
-use app\models\Bagian;
+use app\models\JenisBrg;
 use yii\data\ActiveDataProvider;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
