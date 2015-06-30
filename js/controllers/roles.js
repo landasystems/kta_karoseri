9384b9dd208ffb3282a934b30fb1f8c4af6934fe
x1n73ct <x1n73ct@x1n73ct>
2015-06-30 09:01:54 +0700

    Merge origin/master
    Conflicts:
    	.gitignore

--- js/controllers/roles.js
+++ js/controllers/roles.js
@@ -73,6 +73,9 @@ app.controller('rolesCtrl', function ($scope, Data, toaster) {
         });
     };
     $scope.cancel = function () {
+         if (!$scope.is_view) { //hanya waktu edit cancel, di load table lagi
+            $scope.callServer(tableStateRef);
+        }
         $scope.is_edit = false;
         $scope.is_view = false;
     };
