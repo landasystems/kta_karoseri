35c293ee74c993932ff8bc3f047065dde9dcbdf6
asega <asega@komputer-unyu>
2015-07-15 08:40:25 +0700

    Merge origin/master
    Conflicts:
    	api/controllers/StokkeluarController.php

--- js/controllers/s-pesanankaroseri.js
+++ js/controllers/s-pesanankaroseri.js
@@ -134,7 +134,7 @@ app.controller('spkaroseriCtrl', function($scope, Data, toaster) {
         $scope.is_create = false;
         $scope.formtitle = "Edit Data : " + form.no_spk;
         $scope.form = form;
-        $scope.selected(form.no_retur_bbk);
+        $scope.selected(form.no_spk);
     };
     $scope.view = function(form) {
         $scope.is_edit = true;
@@ -142,7 +142,7 @@ app.controller('spkaroseriCtrl', function($scope, Data, toaster) {
         $scope.is_create = false;
         $scope.formtitle = "Lihat Data : " + form.no_spk;
         $scope.form = form;
-        $scope.selected(form.no_retur_bbk);
+        $scope.selected(form.no_spk);
     };
     $scope.save = function(form) {
         var url = ($scope.is_create == true) ? 'spkaroseri/create' : 'spkaroseri/update/' + form.no_retur_bbk;
@@ -167,18 +167,9 @@ app.controller('spkaroseriCtrl', function($scope, Data, toaster) {
             });
         }
     };
-    $scope.selected = function(id) {
-        Data.get('returbbk/view/' + id).then(function(data) {
+    $scope.selected = function(no_spk) {
+        Data.get('spkaroseri/view/' + no_spk).then(function(data) {
             $scope.form = data.data;
-            if (jQuery.isEmptyObject(data.detail)) {
-                $scope.detailBbk = [{
-                        kd_barang: '',
-                        jml: '',
-                        ket: '',
-                    }];
-            } else {
-                $scope.detailBbk = data.detail;
-            }
         });
     }
 })
