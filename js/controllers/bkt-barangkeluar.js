d3765ebdf19cddeb5f99a7fb5d98ac2fa94a504a
asega <asega@komputer-unyu>
2015-07-23 15:46:52 +0700

    Merge origin/master
    Conflicts:
    	js/config.router.js

--- js/controllers/bkt-barangkeluar.js
+++ js/controllers/bkt-barangkeluar.js
@@ -29,7 +29,7 @@ app.controller('bbkCtrl', function($scope, Data, toaster) {
 
     $scope.cariJabatan = function($query) {
         if ($query.length >= 3) {
-            Data.get('jabatan/listjabatan', {nama: $query}).then(function(data) {
+            Data.get('jabatan/cari', {nama: $query}).then(function(data) {
                 $scope.resultsjabatan = data.data;
             });
         }
