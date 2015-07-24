c3d8e477afb5d3a12c9aa81bbff83161fc8c9c04
asega <asega@komputer-unyu>
2015-07-24 08:32:40 +0700

    Merge origin/master
    Conflicts:
    	js/config.router.js

--- js/controllers/kpb.js
+++ js/controllers/kpb.js
@@ -2,6 +2,8 @@ app.controller('kpbCtrl', function($scope, Data, toaster) {
     $scope.jabatan = [];
     $scope.form = [];
     $scope.detail = [];
+    $scope.status = 0;
+    $scope.msg = '';
 
     $scope.cariWo = function($query) {
         if ($query.length >= 3) {
@@ -12,6 +14,8 @@ app.controller('kpbCtrl', function($scope, Data, toaster) {
     }
 
     $scope.listBagian = function($query) {
+        $scope.jabatan = [];
+        $scope.detail = [];
         if ($query.length >= 3) {
             Data.get('kpb/jabkpb', {key: $query}).then(function(data) {
                 $scope.jabatan = data.data;
@@ -21,11 +25,13 @@ app.controller('kpbCtrl', function($scope, Data, toaster) {
 
     $scope.listBahan = function(kd_bom, kd_jab) {
         var dt = {
-            kd_bom: kd_bom.kd_bom,
+            kd_bom: kd_bom,
             kd_jab: kd_jab,
         };
         Data.post('kpb/listbahan', dt).then(function(data) {
             $scope.detail = data.data;
+            $scope.status = data.print;
+            $scope.msg = data.msg;
         });
     }
 })
