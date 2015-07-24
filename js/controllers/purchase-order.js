f048c5ca6c606c4a83bcc905f67e5b2f4a2aeb0c
x1n73ct <x1n73ct@x1n73ct>
2015-07-24 15:25:07 +0700

    Merge origin/master
    Conflicts:
    	tpl/t_penjualan/index.html

--- js/controllers/purchase-order.js
+++ js/controllers/purchase-order.js
@@ -33,7 +33,13 @@ app.controller('poCtrl', function ($scope, Data, toaster) {
     Data.get('po/listsupplier').then(function (data) {
         $scope.listsupplier = data.data;
     });
-    
+
+
+    $scope.updt_st = function ($id) {
+        Data.get('po/updtst/'+$id).then(function (data) {
+//            $scope.callServer(tableStateRef);
+        });
+    }
 
     $scope.cariSpp = function ($query) {
 
@@ -92,14 +98,9 @@ app.controller('poCtrl', function ($scope, Data, toaster) {
         } else {
             var nilai_ppn = 0;
         }
-//        if ($scope.form.bayar)
-
-//        var total_bayar = $scope.form.total_dibayar;
         var total_dp = $scope.form.dp;
         var total_seluruh = ((total - nilai_diskon) + nilai_ppn);
         var sisa_bayar = (total_seluruh - total_dp);
-        
-//        console.log(total_seluruh);
 
         $scope.form.ppn = Math.ceil(nilai_ppn);
         $scope.form.diskon = Math.ceil(nilai_diskon);
@@ -107,6 +108,7 @@ app.controller('poCtrl', function ($scope, Data, toaster) {
         $scope.form.sisa_dibayar = Math.ceil(sisa_bayar);
 
     }
+
 
 
     $scope.addDetail = function () {
@@ -125,14 +127,14 @@ app.controller('poCtrl', function ($scope, Data, toaster) {
 
 //detail
     $scope.detsPo = {
-            nota: '',
-            kode_barang: '',
-            jml: '',
-            harga: '',
-            diterima: '0',
-            ket: '',
-            tgl_pengiriman: ''
-        };
+        nota: '',
+        kode_barang: '',
+        jml: '',
+        harga: '',
+        diterima: '0',
+        ket: '',
+        tgl_pengiriman: ''
+    };
 
 //remove
     $scope.removeRow = function (paramindex) {
@@ -164,6 +166,60 @@ app.controller('poCtrl', function ($scope, Data, toaster) {
         $scope.openedDet = $index;
     };
 
+    var satuan = {0: "nol", 1: "satu", 2: "dua", 3: "tiga", 4: "empat", 5: "lima", 6: "enam", 7: "tujuh", 8: "delapan", 9: "sembilan"};
+    var belasan = {10: "sepuluh", 11: "sebelas", 12: "dua belas", 13: "tiga belas", 14: "empat belas", 15: "lima belas", 16: "enam belas", 17: "tujuh belas", 18: "delapan belas", 19: "sembilan belas"};
+    var puluhan = {2: "dua puluh", 3: "tiga puluh", 4: "empat puluh", 5: "lima puluh", 6: "enam puluh", 7: "tujuh puluh", 8: "delapan puluh", 9: "sembilan puluh"};
+    var sekala = [
+        {name: "sptiliun", size: 24},
+        {name: "sextiliun", size: 21},
+        {name: "quintiliun", size: 18},
+        {name: "quadriliun", size: 15},
+        {name: "triliun", size: 12},
+        {name: "milyar", size: 9},
+        {name: "juta", size: 6},
+        {name: "ribu", size: 3},
+        {name: "ratus", size: 2}
+    ];
+
+    $scope.keKata = function (num) {
+        var parts = [], minusStr = "";
+        var satuantr = num.toString();
+
+        if (satuantr.length < 1) {
+            return "";
+        }
+
+        if (satuantr[0] == " ") {
+            minusStr = "minus ";
+            satuantr = satuantr.substring(1);
+        }
+
+        for (var i = 0; i < sekala.length; i++) {
+            var scale = sekala[i];
+            if (satuantr.length > scale.size) {
+                var mag = satuantr.length - scale.size;
+                parts.push($scope.keKata(satuantr.substring(0, mag)) + " " + scale.name);
+                satuantr = satuantr.substring(mag).replace(/^[0]+/, '');
+            }
+        }
+
+        num = parseInt(satuantr, 10);
+
+        if (num >= 20) {
+            var puluhantr = puluhan[Math.floor(num / 10)];
+            if (num % 10 !== 0) {
+                puluhantr += " " + satuan[num % 10];
+            }
+            parts.push(puluhantr);
+        } else if (num >= 10) {
+            parts.push(belasan[num]);
+        } else if (num >= 0) {
+            parts.push(satuan[num]);
+        }
+
+        var lastPart = parts.pop();
+        return minusStr + (parts.length > 0 ? parts.join(", ") + "  " : "") + lastPart;
+    }
 
 //button
     $scope.create = function (form) {
@@ -241,6 +297,7 @@ app.controller('poCtrl', function ($scope, Data, toaster) {
     $scope.selected = function (id) {
         Data.get('po/view/' + id).then(function (data) {
             $scope.form = data.data;
+            $scope.form.terbilang = $scope.keKata(data.data.total_dibayar) + ' RUPIAH';
             $scope.detsPo = data.detail;
             $scope.form.dp = (data.data.dp == undefined) ? '0' : data.data.dp;
             $scope.form.ppn = (data.data.ppn == undefined) ? '0' : data.data.ppn;
