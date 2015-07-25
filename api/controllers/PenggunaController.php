bfb4e3f6ea27f2e3a853fab4c654b33d785c9d33
yuliantofrandi <yuliantofrandi@laptop1>
2015-07-25 14:58:49 +0700

    Merge origin/master
    Conflicts:
    	js/config.router.js

--- api/controllers/PenggunaController.php
+++ api/controllers/PenggunaController.php
@@ -140,20 +140,6 @@ class PenggunaController extends Controller {
         }
     }
 
-    public function actionRoles() {
-        $query = new Query;
-        $query->from('m_roles')
-                ->select('*')
-                ->where(['is_deleted'=>0]);
-
-        $command = $query->createCommand();
-        $models = $command->queryAll();
-
-        $this->setHeader(200);
-
-        echo json_encode(array('status' => 1, 'roles' => $models));
-    }
-
     public function actionUpdate($id) {
         $params = json_decode(file_get_contents("php://input"), true);
         $model = $this->findModel($id);
