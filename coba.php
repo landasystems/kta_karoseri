60f7da9bedc834e1fd3402474ab253bba7c723c0
x1n73ct <x1n73ct@x1n73ct>
2015-10-27 09:19:22 +0700

    Merge origin/master
    Conflicts:
    	api/controllers/KaryawanController.php

--- coba.php
+++ coba.php
@@ -1,10 +1,31 @@
 <?php
-if (date_default_timezone_get()) {
-    echo 'date_default_timezone_set: ' . date_default_timezone_get() . '<br />';
-}
 
-if (ini_get('date.timezone')) {
-    echo 'date.timezone: ' . ini_get('date.timezone');
+function login() {
+    $username = $this->security->xss_clean($this->input->post('username'));
+    $password = $this->security->xss_clean($this->input->post('password'));
+
+    $this->db->where('userName', $username);
+    $this->db->where('password', $password);
+
+    $q = $this->db->query("select * from userDetail, prvUser where userDetail.userID = prvUser.userID AND userName like '" . $username . "'");
+
+    if ($query->num_rows == 1) {
+        $row = $query->row();
+        $data = array(
+            'userID' => $row->userID,
+            'session_id' => $row->userID,
+            'userName' => $row->userName,
+            'CD' => "$row->CreateDashboard",
+            'ED' => $row->EditDashboard,
+            'DD' => $row->DeleteDashboard,
+            'CP' => $row->CreatePanel,
+            'EP' => $row->EditPanel,
+            'DP' => $row->DeletePanel,
+            'validated' => true
+        );
+        $this->session->set_userdata($data);
+        return true;
+    }
+    return false;
 }
-?>
-haii
+    ?>
