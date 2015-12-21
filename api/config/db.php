afbee44e5c1dcc0cfa855b5ba0732f096e2e1286
x1n73ct <x1n73ct@x1n73ct>
2015-12-21 13:14:11 +0700

    Merge origin/master
    Conflicts:
    	api/controllers/PekerjaanController.php

--- api/config/db.php
+++ api/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
 //    'dsn' => 'mysql:host=localhost;dbname=landa2_cms',
-    'dsn' => 'mysql:host=localhost;dbname=landa_kta_karoseri',
+    'dsn' => 'mysql:host=localhost;dbname=purchassing2',
     'username' => 'root',
     'password' => 'landak',
     'charset' => 'utf8',
